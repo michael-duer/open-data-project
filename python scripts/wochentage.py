61aed3dd743944aed0e76a1d5b28424383ca0af9
michael.duer <62062370+michael-duer@users.noreply.github.com>
2021-05-16 18:57:43 +0200

    add python script for heatmap data
    add script that reads a csv file with passenger per day data and creates a new file with columns weekday, month and passenger amount

--- python scripts/wochentage.py
+++ python scripts/wochentage.py
@@ -1,16 +1,22 @@
 from datetime import datetime
+import csv
 
-# Create datetime object
-date='2019-01-01'
+#init reader
+csv_read = open('./data/year2020.csv', 'r')
+reader = csv.reader(csv_read)
 
-# Define a list of weekday names
-days = ['Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag', 'Sonntag']
+#init writer
+with open('heatmap2020.csv', 'w', newline='') as csv_write:
+    writer = csv.writer(csv_write, delimiter=',', quoting = csv.QUOTE_MINIMAL)
 
-#string to date type
-date = datetime.strptime(date, '%Y-%m-%d')
+    next(reader) #skip header in read file
+    writer.writerow(['day,month,value']) #write header
+ 
+    for row in reader:
+        date = datetime.strptime(row[0], '%d.%m.%Y') #parse date
+        weekday = date.weekday()
+        month = date.month
+        value = row[1]
+        writer.writerow([str(weekday), str(month), str(value)])
 
-# Index using weekday int value
-weekday = days[date.weekday()]
-
-
-print(weekday)
+print('File generated!')
