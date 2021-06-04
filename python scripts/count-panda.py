eaafd85eeef5d8d3db5f530d76dbd8746b094347
michael.duer <62062370+michael-duer@users.noreply.github.com>
2021-06-04 12:14:57 +0200

    delete unused python code and script

--- python scripts/count-panda.py
+++ python scripts/count-panda.py
@@ -1,29 +1,15 @@
 import pandas as pd
-import csv
 from datetime import datetime
 
-csv_file = pd.read_csv('./data/Fahrgastzahlen_2019-2.csv', index_col=False)
+csv_file = pd.read_csv('./data/Fahrgastzahlen_2019.csv', index_col=False)
 
-#format the 'date' column as date type in the form: "year.month.day"
-#csv_file['date'] = pd.to_datetime(csv_file.date, format="%Y.%m.%d", infer_datetime_format = True)
+#format the 'date' column as date type in the form: "day.month.year"
 pd.to_datetime(csv_file['date'], format="%d.%m.%Y")
-#datetimeobject = datetime.strptime(csv_file['date'],'%d.%m.%Y')
-
-#wrong output!!
-#csv_file['date'] = pd.to_datetime(csv_file['date']).dt.strftime('%d.%m.%Y')
-
-#print(csv_file)
 
 #group dates, remove and add up dublicates of transported_people
 edited_file = csv_file.groupby(['date'])['transported_people'].agg(['sum'])
 
-#sortierung anpassen so dass datum reihenfolge stimmt
-#sortiert = sorted(df1, key=lambda row: row[0], reverse=True)
-#df['Date'] = pd.to_datetime(df['Date']).dt.strftime('%d/%m/%Y')
-
-#edited_file = sorted(edited_file, key = lambda row: datetime.strptime(row[0], "%d.%m.%Y"))
-edited_file.sort_values(by="date", ascending = True, inplace=True)
 #export as csv
-edited_file.to_csv("./data/frequenz-year2019.csv")
+edited_file.to_csv("./data/test.csv")
 
 print("Calculation finished!")
--- python scripts/replace.py
+++ python scripts/replace.py
@@ -1,17 +1,8 @@
 import csv
 
-reader = csv.reader(open("./data/Fahrgastzahlen_2019.csv", "r"), delimiter=';')
+reader = csv.reader(open("./pathToFile/oldFile.csv", "r"), delimiter=';')
 
-writer = csv.writer(open("./data/Fahrgastzahlen_2019-2.csv", 'w', newline=''), delimiter=',')
+writer = csv.writer(open("./pathToFile/newFile.csv", 'w', newline=''), delimiter=',')
 writer.writerows(reader)
 
 print("Delimiter successfully changed")
-
-
-
-
-#import pandas as pd
-
-#newCsv = pd.read_csv('Fahrgastzahlen_2019.csv',sep=';',decimal=',')
-
-#newCsv.to_csv("real-test.csv")
